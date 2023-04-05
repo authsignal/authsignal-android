205933acd395054864516c48a618a7e700e2afb2
Chris Fisher <chris.i.fisher@gmail.com>
2023-04-05 13:09:38 +1200

    Update gradle publishing config (#3)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
 plugins {
+  id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
   id("org.jetbrains.kotlin.plugin.serialization") version "1.6.21"
 }
 
@@ -18,5 +21,19 @@ allprojects {
   repositories {
     google()
     mavenCentral()
+  }
+}
+
+nexusPublishing {
+  repositories {
+    sonatype {
+      val properties = gradleLocalProperties(rootDir)
+
+      username.set(properties.getProperty("ossrhUsername"))
+      password.set(properties.getProperty("ossrhPassword"))
+      stagingProfileId.set(properties.getProperty("sonatypeStagingProfileId"))
+      nexusUrl.set(uri(properties.getProperty("nexusUrl")))
+      snapshotRepositoryUrl.set(uri(properties.getProperty("snapshotRepositoryUrl")))
+    }
   }
 }
