2f38c33c9d73b753f2d67bbabd25ddb7ed17d269
Chris Fisher <chris.i.fisher@gmail.com>
2024-08-21 15:13:52 +1200

    Add more fields to push challenge response (#25)

---
+++ module/src/main/java/com/authsignal/push/models/PushChallenge.kt
@@ -0,0 +1,10 @@
+package com.authsignal.push.models
+
+data class PushChallenge(
+  val challengeId: String,
+  val actionCode: String?,
+  val idempotencyKey: String?,
+  val deviceId: String?,
+  val userAgent: String?,
+  val ipAddress: String?,
+)
