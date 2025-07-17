dd6100e51d498355f125a24dde050c99b04214c8
Chris Fisher <chris.i.fisher@gmail.com>
2025-07-17 14:11:12 +1200

    Use jReleaser for publish workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,41 +1,27 @@
-import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
-
 plugins {
-  id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
-  id("org.jetbrains.kotlin.plugin.serialization") version "1.6.21"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.6.21"
 }
 
 buildscript {
-  repositories {
-    mavenCentral()
-    google()
-    maven { url = uri("https://repo.gradle.org/gradle/libs-releases") }
-  }
-  dependencies {
-    classpath("com.android.tools.build:gradle:8.1.4")
-    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
-  }
+    repositories {
+        mavenCentral()
+        google()
+        maven { url = uri("https://repo.gradle.org/gradle/libs-releases") }
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.4")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
+    }
+    configurations.all {
+        resolutionStrategy {
+            force("org.apache.commons:commons-compress:1.26.2")
+        }
+    }
 }
 
 allprojects {
-  repositories {
-    google()
-    mavenCentral()
-  }
-}
-
-fun getProperty(propertyName: String, gradleLocalPropertyName: String = propertyName): String {
-  return System.getenv(propertyName) ?: gradleLocalProperties(rootDir).getProperty(gradleLocalPropertyName) ?: ""
-}
-
-nexusPublishing {
-  repositories {
-    sonatype {
-      username.set(getProperty("OSSRH_USERNAME", "ossrhUsername"))
-      password.set(getProperty("OSSRH_PASSWORD", "ossrhPassword"))
-      stagingProfileId.set(getProperty("SONATYPE_STAGING_PROFILE_ID", "sonatypeStagingProfileId"))
-      nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-      snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+    repositories {
+        google()
+        mavenCentral()
     }
-  }
 }
--- module/build.gradle.kts
+++ module/build.gradle.kts
@@ -1,126 +1,161 @@
 import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
-import org.gradle.api.GradleException
 
 plugins {
-  id("com.android.library")
-  id("kotlin-android")
-  id("kotlinx-serialization")
+    id("com.android.library")
+    id("kotlin-android")
+    id("kotlinx-serialization")
+    id("maven-publish")
+    id("org.jreleaser") version "1.19.0"
+}
 
-  `maven-publish`
-  signing
+configurations.all {
+    resolutionStrategy {
+        force("org.apache.commons:commons-compress:1.26.2")
+    }
 }
 
 android {
-  namespace = "com.authsignal"
+    namespace = "com.authsignal"
 
-  compileSdk = 34
+    compileSdk = 34
 
-  defaultConfig {
-    minSdk = 23
-    consumerProguardFiles("consumer-rules.pro")
-  }
-
-  compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-  }
-
-  kotlinOptions {
-    jvmTarget = "17"
-  }
-
-  buildTypes {
-    getByName("release") {
-      isMinifyEnabled = false
-      proguardFiles(
-        getDefaultProguardFile("proguard-android-optimize.txt"),
-        "proguard-rules.pro"
-      )
+    defaultConfig {
+        minSdk = 23
+        consumerProguardFiles("consumer-rules.pro")
     }
-  }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+            getDefaultProguardFile("proguard-android-optimize.txt"),
+            "proguard-rules.pro"
+            )
+        }
+    }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
 }
 
-fun getProperty(propertyName: String, gradleLocalPropertyName: String = propertyName): String {
-  return System.getenv(propertyName) ?: gradleLocalProperties(rootDir).getProperty(gradleLocalPropertyName) ?: ""
+fun getProperty(name: String): String {
+    return System.getenv(name) ?: gradleLocalProperties(rootDir).getProperty(name)
 }
 
 val sourcesJar by tasks.creating(Jar::class) {
-  archiveClassifier.set("sources")
-  from(android.sourceSets.getByName("main").java.srcDirs)
+    archiveClassifier.set("sources")
+    from(android.sourceSets.getByName("main").java.srcDirs)
 }
 
 val pomGroup: String by project
 val pomArtifactId: String by project
 val versionName: String by project
 
+project.version = versionName
+
 publishing {
-  publications {
-    create<MavenPublication>("Authsignal") {
-      groupId = pomGroup
-      artifactId = pomArtifactId
-      version = versionName
+    publications {
+        create<MavenPublication>("release") {
+            groupId = pomGroup
+            artifactId = pomArtifactId
 
-      artifact("$buildDir/outputs/aar/module-release.aar")
-      
-      artifact(sourcesJar)
+            pom {
+                name.set("authsignal-android")
+                description.set("The official Authsignal SDK for Android.")
+                url.set("https://github.com/authsignal/authsignal-android")
+            
+                scm {
+                    url.set("https://github.com/authsignal/authsignal-android/tree/main")
+                    connection.set("scm:git:github.com/authsignal/authsignal-android.git")
+                    developerConnection.set("scm:git:ssh://github.com/authsignal/authsignal-android.git")
+                }
 
-      pom.withXml {
-        asNode().apply {
-          appendNode("name", "authsignal-android")
-          appendNode("description", "The official Authsignal SDK for Android.")
-          appendNode("url", "https://github.com/authsignal/authsignal-android")
-          appendNode("licenses").apply {
-            appendNode("license").apply {
-              appendNode("name", "MIT")
-              appendNode("url", "https://github.com/authsignal/authsignal-android/blob/main/LICENSE.md")
+                licenses {
+                    license {
+                        name.set("MIT")
+                        url.set("https://github.com/authsignal/authsignal-android/blob/main/LICENSE.md")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("Authsignal")
+                        name.set("Authsignal")
+                    }
+                }
+
+                afterEvaluate {
+                    from(components["release"])
+                }
             }
-          }
-          appendNode("developers").apply {
-            appendNode("developer").apply {
-              appendNode("id", "Authsignal")
-              appendNode("name", "Authsignal")
-            }
-          }
-          appendNode("scm").apply {
-            appendNode("connection", "scm:git:github.com/authsignal/authsignal-android.git")
-            appendNode("developerConnection", "scm:git:ssh://github.com/authsignal/authsignal-android.git")
-            appendNode("url", "https://github.com/authsignal/authsignal-android/tree/main")
-          }
-          appendNode("dependencies").apply {
-            project.configurations["releaseImplementation"].allDependencies.forEach {
-              appendNode("dependency").apply {
-                appendNode("groupId", it.group)
-                appendNode("artifactId", it.name)
-                appendNode("version", it.version)
-              }
-            }
-          }
         }
-      }
     }
-  }
+
+    repositories {
+        maven {
+            setUrl(layout.buildDirectory.dir("staging-deploy"))
+        }
+    }
 }
 
-signing {
-  val signingKeyId = getProperty("SIGNING_KEY_ID", "signing.keyId")
-  val signingKey = getProperty("SIGNING_KEY", "signing.key")
-  val signingPassword = getProperty("SIGNING_PASSWORD", "signing.password")
-  
-  if (signingKeyId.isNotEmpty() && signingKey.isNotEmpty() && signingPassword.isNotEmpty()) {
-    useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
-    sign(publishing.publications)
-  } else {
-    throw GradleException("Signing information incomplete. Publishing requires valid signing configuration.")
-  }
+jreleaser {
+    project {
+        java {
+            groupId.set("com.authsignal")
+            artifactId.set("authsignal-android")
+            description.set("The official Authsignal SDK for Android.")
+        }
+    }
+    gitRootSearch = true
+    signing {
+        setActive("ALWAYS")
+        verify = false
+        secretKey = getProperty("SIGNING_SECRET_KEY")
+        passphrase = getProperty("SIGNING_PASSPHRASE")
+    }
+    release {
+        github {
+            skipRelease = true
+        }
+    }
+    deploy {
+        maven {
+            mavenCentral {
+                create("sonatype") {
+                    stagingRepository("build/staging-deploy")
+                    setActive("ALWAYS")
+                    url = "https://central.sonatype.com/api/v1/publisher"
+                    username = getProperty("MAVENCENTRAL_USERNAME")
+                    password = getProperty("MAVENCENTRAL_PASSWORD")
+                    applyMavenCentralRules = false // See: https://github.com/kordamp/pomchecker/issues/21
+                    sourceJar = true
+                    javadocJar = true
+                    sign = true
+                }
+            }
+        }
+    }
 }
 
 val ktorVersion: String by project
 
 dependencies {
-  implementation("io.ktor:ktor-client-android:$ktorVersion")
-  implementation("io.ktor:ktor-client-core:$ktorVersion")
-  implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
-  implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
-  implementation("androidx.credentials:credentials:1.2.2")
-  implementation("androidx.credentials:credentials-play-services-auth:1.2.2")
+    implementation("io.ktor:ktor-client-android:$ktorVersion")
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+    implementation("androidx.credentials:credentials:1.2.2")
+    implementation("androidx.credentials:credentials-play-services-auth:1.2.2")
 }
