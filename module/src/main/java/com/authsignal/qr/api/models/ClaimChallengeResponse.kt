bdc3965fc1ce9d253e6ad96e12d5c8d2cb63095c
Steven Clouston <stevenclouston@gmail.com>
2025-11-18 16:19:02 +1300

    AUT-3058 Return action code and idempotencyKey from claimChallenge

--- module/src/main/java/com/authsignal/qr/api/models/ClaimChallengeResponse.kt
+++ module/src/main/java/com/authsignal/qr/api/models/ClaimChallengeResponse.kt
@@ -7,4 +7,6 @@ data class ClaimChallengeResponse(
   val success: Boolean,
   val userAgent: String? = null,
   val ipAddress: String? = null,
+  val actionCode: String,
+  val idempotencyKey: String,
 ) 
